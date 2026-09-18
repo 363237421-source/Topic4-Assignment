64277255c06fd71dfbb4ad914c2c704b7506d7f3
Said Said <363237421@GoTVDSB.ca>
2026-09-18 13:19:49 -0400

    Finshed for the day

--- Topic4 Assignment/Program.cs
+++ Topic4 Assignment/Program.cs
@@ -28,7 +28,8 @@
             Console.WriteLine("I will be gathering information from you, please answer truthfully.");
 
             string firstName, lastName, login;
-            int grade, average;
+            int grade, ID;
+            double average;
 
             Console.WriteLine("First name please.");
             firstName = Console.ReadLine();
@@ -36,13 +37,63 @@
             lastName = Console.ReadLine();
             Console.WriteLine("Login account please.");
             login = Console.ReadLine();
+            Console.WriteLine("What is your ID number?");
+            Int32.TryParse (Console.ReadLine(), out ID);
+            Console.WriteLine("What grade are you? (9-12)");
+            Int32.TryParse(Console.ReadLine(),out grade);
+            while (grade < 8)
+            {
+                Console.WriteLine("Sorry but your grade must be over 8, please try again!");
+                Int32.TryParse(Console.ReadLine(), out grade);
+            }
+            Console.WriteLine("What are you averaging right now?");
+            Double.TryParse(Console.ReadLine()  ,out average);
+            Console.WriteLine("Thank you for your information! =)");
+            Console.ReadLine();
+            Console.Clear();
 
+            Console.WriteLine("Your information:");
+            Console.WriteLine("\t Login: " + login);
+            Console.WriteLine("\t Name: " + firstName + " " + lastName);
+            Console.WriteLine("\t Grade: " + grade);
+            Console.WriteLine("\t ID: " + ID);
+            Console.WriteLine("\t Average: " + average + "%");
+            Console.ReadLine();
+            Console.Clear();
+
+
+
+        }
+
+        public static void part3()
+        {
+            string name;
+            int age;
+
+            Console.Write("Hi there, what is your name? ");
+            name = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Hello, " + name + "." + " How old are you?");
+            Int32.TryParse(Console.ReadLine(), out age);
+            Console.WriteLine();
+
+            Console.WriteLine("Did you know in five years you will be " + (age + 5) + " years old?");
+            Console.WriteLine("And five years ago you were " + (age - 5) + "! Imagine that!");
+            Console.ReadLine();
+            Console.Clear();
         }
 
         public static void Main(string[] args)
         {
 
+            part3();
+
+            Console.ReadLine();
+
             part2();
+
+            Console.ReadLine();
 
             part1();
          
